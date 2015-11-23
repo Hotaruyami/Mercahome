c9ac8c5b34355cfb41e6bc913183d6fe87de1222
Hotaruyami <soriano.tasso@gmail.com>
2015-11-23 18:32:13 +0100

    A veure que et sembla, no espero que estigui bé, a part és llarg i inneficient segur pero, potser t’inspira o algo? xD

--- caixa.cc
+++ caixa.cc
@@ -4,11 +4,11 @@
 caixa::caixa(int estat){
 	maxcua = 20;
 	cua = queue<client>(maxcua);
-	hora = "00:00:00”;
+	hora = "00:00:00";
 	this->estat = estat;
 	
-
 }
+
 caixa::~caixa(){}
 
 void caixa::cobrar_client(){
@@ -33,24 +33,57 @@ int caixa::estat_caixa(){
 }
 // esta bastan liada la cosa nose com agafar el string i sumarlo xd, miratho aver.
 string caixa::temps_de_la_cua(string temps){
-	string aux,auxhora;
-	int aux1,inthora;
-	for (int i = 0; i < ;++i){
-		aux = temps[i]+temps[i+1];
-		aux1 = int(aux);
-		auxhora = hora[i]+hora[i+1];
-		inthora = int(auxhora);
-		if (aux1 + auxhora < 60){
-		aux1 = aux1 + auxhora;
-		hora = hora + str(aux1) + ":";
+	//hora = hora + temps
+	string auxSH,auxSM,auxSS,horastr;
+	int auxIH,auxIM,auxIS,horaint,aux;
+
+		auxSH = temps[0] + temps[1];
+		auxSM = temps[3] + temps[4];
+		auxSS = temps[6] + temps[7];
+		auxIH = int(auxSH);
+		auxIM = int(auxSM);
+		auxIS = int(auxSS);
+
+		horastr = hora[6] + hora[7];
+		horaint = int(horastr);
+		if((horaint + auxIS) < 60){
+			aux = horaint + auxIS;
+			hora[7] = str(aux%10); 
+			aux = aux/10;
+			hora[6] = str(aux%10);
 		}
-		else {
-		if (i = 0) i = ;
 		else{
-		aux1 = (aux1+auxhora)mod60
-		}
-		}	
+			hora[6] = "0";
+			hora[7] = "0";
+			++auxIM;
+		} 
 		
+		horastr = hora[3] + hora[4];
+		horaint = int(horastr);
+		if((horaint + auxIM) < 60){
+			aux = horaint + auxIM;
+			hora[4] = str(aux%10); 
+			aux = aux/10;
+			hora[3] = str(aux%10);
+		}
+		else{
+			hora[3] = "0";
+			hora[4] = "0";
+			++auxIH;
+		} 
+		
+		horastr = hora[0] + hora[1];
+		horaint = int(horastr);
+		if((horaint + auxIH) < 24){
+			aux = horaint + auxIH;
+			hora[1] = str(aux%10); 
+			aux = aux/10;
+			hora[0] = str(aux%10);
+		}
+		else{
+			//La hora pot ser major a 24?
+			//No sé si funciona, bastant llarg i inneficient. Pero més o menys és aquesta la idea no?
+		} 
+		return hora;
 	
 	}
-}

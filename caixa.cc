48c026d26e8b5344f9c77aaea6035a6dc54c4684
Miki <mikitienevista@gmail.com>
2015-11-23 17:40:31 +0100

    23/11/2015

--- caixa.cc
+++ caixa.cc
@@ -1,4 +1,5 @@
 #include "caixa.hh"
+#include <string>
 
 caixa::caixa(int estat){
 	maxcua = 20;
@@ -30,7 +31,26 @@ void caixa::esborrar_client(){
 int caixa::estat_caixa(){
 	return estat;
 }
-
+// esta bastan liada la cosa nose com agafar el string i sumarlo xd, miratho aver.
 string caixa::temps_de_la_cua(string temps){
-
+	string aux,auxhora;
+	int aux1,inthora;
+	for (int i = 0; i < ;++i){
+		aux = temps[i]+temps[i+1];
+		aux1 = int(aux);
+		auxhora = hora[i]+hora[i+1];
+		inthora = int(auxhora);
+		if (aux1 + auxhora < 60){
+		aux1 = aux1 + auxhora;
+		hora = hora + str(aux1) + ":";
+		}
+		else {
+		if (i = 0) i = ;
+		else{
+		aux1 = (aux1+auxhora)mod60
+		}
+		}	
+		
+	
+	}
 }
--- main.cc
+++ main.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+El#include <iostream>
 using namespace std;
 
 int main() {

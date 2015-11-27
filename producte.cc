ee90e3b91c7268cad2476a31b22cec91fc2cd345
Roger <soriano.tasso@gmail.com>
2015-11-27 08:58:43 +0100

    Presentació

--- producte.cc
+++ producte.cc
@@ -8,10 +8,7 @@ producte::producte(){
 }
 
 producte::producte(string nom, double preu, int tempsc, string seccio){
-	
-	
 	if (tempsc < 0 or preu < 0 or seccio.size() == 2) cout << "error" << endl;
-
 	else {
 		this->nom = nom;
 		this->preu = preu;
@@ -42,11 +39,8 @@ void producte::llegir(){
 	cin >> nom;
 	double aux,aux2;
 	string aux3;
-	if (aux >= 0 and aux2 >= 0 and aux3.size() == 2){
 	cin >> aux >> aux3 >> aux2;
-	}
-	else cout << "error" << endl;
-	
+	if (aux < 0 and aux2 < 0 and aux3.size() != 2) cout << "error" << endl;
 }
 void producte::escriure(){
 	cout << nom << preu << seccio << tempsc << endl;
